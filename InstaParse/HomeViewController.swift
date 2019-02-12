e6b0635dec25518a4a42ea2685ea92a2a333ca21
Yong Dong <yongdong11467@gmail.com>
2019-02-11 22:10:41 -0500

    added login auth

--- InstaParse/HomeViewController.swift
+++ InstaParse/HomeViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
     
 
--- InstaParse/LoginViewController.swift
+++ InstaParse/LoginViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func didSignIn(_ sender: UIButton) {
+        PFUser.logInWithUsername(inBackground: userNameField.text!, password: passwordField.text!) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+            } else {
+                print(error as Any)
+            }
+        }
+    }
+    
+    @IBAction func didSignUp(_ sender: UIButton) {
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+            } else {
+                print(error as Any)
+            }
+        }
+        
+    }
+    
 }
 
