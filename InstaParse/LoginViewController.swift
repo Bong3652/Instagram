a6197ce4127a99a8dd60071a1ca54ca789881a59
Yong Dong <yongdong11467@gmail.com>
2019-02-11 19:01:30 -0500

    first commit

---
+++ InstaParse/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaParse
+//
+//  Created by APPLE on 2/11/19.
+//  Copyright © 2019 Bong. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
